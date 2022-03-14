35b8f027c630259428526ffda6b841056ff02ce9
Timmy254 <TimothyMwangi6911@gmail.com>
2022-03-14 12:29:05 -0700

    print alphabet in lowercase followed by newline

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,14 +12,3 @@ putchar(ch);
 putchar('\n');
 return 0;
 }
-int main(void)
-{    
-for (char ch ='a'; ch <= 'z'; ch++)
-{
-if (ch == 'e' || ch == 'q')
-ch++;
-putchar(ch);
-}
-putchar('\n');
-return 0;
-}
