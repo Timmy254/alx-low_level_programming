3f42c194e4c3e053b242164f2287277ac614dcca
Timmy254 <TimothyMwangi6911@gmail.com>
2022-03-14 13:25:12 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
 int main(void)
 {
-  char ch = 'a';
-  while(ch <= 'z')
-    {
-      if (ch != 'e' && ch != 'q')
-	{
-	  putchar(ch);
-	}
-      ch++;
-    }
-  putchar('\n');
-return 0;
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-for (char ch = 'a'; ch <= 'z'; ch++)
+char ch = 'a';
+while(ch <= 'z')
 {
 if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
 }
-}
-putchar('\n');
-return 0;
-}
-int main(void)
-{    
-for (char ch ='a'; ch <= 'z'; ch++)
-{
-if (ch == 'e' || ch == 'q')
 ch++;
-putchar(ch);
 }
 putchar('\n');
-return 0;
+return (0);
 }
