168c56f7e6a3f7229310e72d6d4828c0f111edae
Timmy254 <Timothymwangi6911@gmail.com>
2022-03-18 23:48:44 +0300

    Check if number is uppercase or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,8 @@
 /**
  * _isupper(int c) - checks if an a character is capital by converting to interger
  * implements the unicode table to be able to determine.
- * Return: 1 if caps 
- * else: return 0
+ * Return: 1 if caps
+ * Always: return 0
  */
 
 int _isupper(int c)
@@ -13,7 +13,6 @@ if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
-
 else
 {
 return (0);
