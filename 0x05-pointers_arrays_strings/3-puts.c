77d0bc1dfb43b4041c30db57d5aa226292909f0d
Timmy254 <Timothymwangi6911@gmail.com>
2022-03-22 21:24:42 +0300

    function to print string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to the output.
+ * @str: the string to be printed.
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
