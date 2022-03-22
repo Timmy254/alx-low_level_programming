fd7b8172ca0200766101516f85e13e1107f19a64
Timmy254 <Timothymwangi6911@gmail.com>
2022-03-22 21:11:11 +0300

    reset using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - parameter taken is a pointer to an int
+ *  then updates it's value to 98.
+ *
+ *  Return: void.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+#C - Pointers, arrays and strings
+
+This section emphasizes on the above to determine how various computations,
+can be performed in the above using C low-level programming language,
+
+
