78872ec70c6c6900fcfac7975c116f2233cfcba9
Timmy254 <Timothymwangi6911@gmail.com>
2022-03-22 21:16:25 +0300

    reset using pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - parameter taken is a pointer to an int
- *  then updates it's value to 98.
- *
+ * then updates it's value to 98.
+ * @n: this is the pointer argument passed in
  *  Return: void.
  */
 void reset_to_98(int *n)
