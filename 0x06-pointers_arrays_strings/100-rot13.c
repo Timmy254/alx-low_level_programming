a77938c64420feaf32149a1d3b94d295d6d050be
Timmy254 <Timothymwangi6911@gmail.com>
2022-03-25 22:48:16 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - implemented to encode a string using rot13.
+ * @s: the string to encode.
+ * Return: char s
+ */
+char *rot13(char *s)
+{
+int i, j;
+
+char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (i = 0; s[i] != '\0'; i++)
+for (j = 0; a[j] != '\0'; j++)
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
+}
+return (s);
+}
